cb058575a51c2760b8af32785b75a276200a17ad
Dawnbee <dawnbeeh23@gmail.com>
2025-07-01 02:33:44 -0400

    editted my intorduce

--- src/components/sections/About/About.jsx
+++ src/components/sections/About/About.jsx
@@ -4,9 +4,12 @@ const About = () => (
   <section id="about" className={styles.about}>
     <h2>About Me</h2>
     <p>
-    Lately, I've been exploring internship opportunities and thinking about what areas of software truly excite me.
-    I'm still in the process of figuring things out, but I've been enjoying the chance to learn and try new things.
-    With summer break giving me a bit more breathing room, I’m taking this time to move at my own pace - and I'm grateful for it.
+    Lately, I've been exploring internship opportunities 
+    and thinking about what areas of software truly excite me.
+    I'm still in the process of figuring things out, 
+    but I've been enjoying the chance to learn and try new things.
+    With summer break giving me a bit more breathing room, 
+    I’m taking this time to move at my own pace - and I'm grateful for it!
     </p>
   </section>
 )
