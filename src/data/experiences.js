ac36a1e47491dc9a6f6bc7d617dd329cd762a3d0
Dawnbee <dawnbeeh23@gmail.com>
2025-07-01 01:22:36 -0400

    added custom experiences

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,16 +1,22 @@
 const experiences = [
   {
-    company: "Company A",
-    role: "role",
-    period: "period",
-    description: "description"
+    "company": "Middlesex Community College",
+    "role": "SI Leader",
+    "period": "09-01-2023 – Present",
+    "description": "Led support sessions for Discrete Math and Precalculus, gave mini lectures, answered questions during weekly tutoring, and assisted with high school math labs at Ramps Up."
   },
   {
-    company: "Company B",
-    role: "role",
-    period: "period",
-    description: "description"
+    "company": "Middlesex Community College",
+    "role": "English Facilitator",
+    "period": "03-06-2023 – 12-23-2023",
+    "description": "Facilitated weekly academic discussions, encouraged student debates, and led English small talk sessions as an icebreaker for ELL students."
   },
+  {
+    "company": "Sasaro",
+    "role": "Business Manager",
+    "period": "03-01-2022 – 05-31-2023",
+    "description": "Worked on business development for a startup, redesigned the website, published weekly news posts, handled HR tasks, wrote business plans, promoted on donation platforms, and helped with office relocation."
+  }
 ]
 
 export default experiences 
