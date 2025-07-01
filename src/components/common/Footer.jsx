7f373d52b2097c6f09de584c129302801d82e872
Dawnbee <dawnbeeh23@gmail.com>
2025-07-01 15:45:39 -0400

    added contact info

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,6 +4,23 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <small>© {new Date().getFullYear()} Dawnbee. Thanks for visiting!</small>
+      <div className={styles.links}>
+        <a
+          href="https://github.com/dawnbeeh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/dawnbee-hwang-aaa861302/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a href="mailto:dawnbeeh23@gmail.com">Email</a>
+      </div>
     </footer>
   )
 }
