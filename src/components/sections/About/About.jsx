4ce843f7b8debabcaf1dcec577b27aafbde7e2bf
Dawnbee <dawnbeeh23@gmail.com>
2025-07-01 00:34:18 -0400

    created simple About section

---
+++ src/components/sections/About/About.jsx
@@ -0,0 +1,12 @@
+import styles from "./About.module.css"
+
+const About = () => (
+  <section id="about" className={styles.about}>
+    <h2>About Me</h2>
+    <p>
+      Hello
+    </p>
+  </section>
+)
+
+export default About 
