8f46871147c15cec6e267f92e800717010b565bf
Dawnbee <dawnbeeh23@gmail.com>
2025-07-01 02:06:48 -0400

    changed message in about me

--- src/components/sections/About/About.jsx
+++ src/components/sections/About/About.jsx
@@ -4,7 +4,9 @@ const About = () => (
   <section id="about" className={styles.about}>
     <h2>About Me</h2>
     <p>
-        I'm a developer-in-progress who enjoys designing systems and building them from the ground up.
+    Lately, I've been exploring internship opportunities and thinking about what areas of software truly excite me.
+    I'm still in the process of figuring things out, but I've been enjoying the chance to learn and try new things.
+    With summer break giving me a bit more breathing room, I’m taking this time to move at my own pace - and I'm grateful for it.
     </p>
   </section>
 )
