58081530fdf3340188362c7b041fa8887dbd0499
Dawnbee <dawnbeeh23@gmail.com>
2025-07-01 00:30:00 -0400

    created Navbar

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,16 @@
+import styles from "./Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#projects">Projects</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar 
