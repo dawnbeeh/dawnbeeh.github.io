fccdb51ebf8c48a68711e8013baa843d51159773
Dawnbee <dawnbeeh23@gmail.com>
2025-07-04 18:20:28 -0400

    add image for Canvas Clone ; editted image link for Figma

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,7 +9,7 @@ const projects = [
     title: "Restaurant UI Design",
     description: "Designed a modern restaurant website using Figma",
     link: "https://www.figma.com/proto/SjSMznbgZyYBpKo8aaDXHj/Hwang_final?node-id=4007-509&t=Rv98GDHB6CfUdBQG-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=4007%3A509",
-    image: "/src/assets/images/Figma-restuarant-UI.png"
+    image: "/src/assets/images/Figma/Figma-restuarant-UI.png"
   },
   {
     title: "Student Math League",
@@ -27,7 +27,7 @@ const projects = [
     title: "Canvas Clone Web App",
     description: "Learning management system clone built with React & TypeScript.",
     link: "",
-    image: ""
+    image: "/src/assets/images/CanvasClone/DashBoard.png"
   },
   {
     title: "Advanced Calculator",
